840dff7efc7eb8884f15fb8180e48e46c1743525
Carlos Cruz <20275455@studentmail.ul.ie>
2024-02-24 14:13:34 +0000

    tidying up code

---
+++ resources/views/Layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Include Tailwind CSS -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container mx-auto">
+        @yield('content')
+    </div>
+</body>
+</html>
--- resources/views/LearnMore.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <h1>Learn More</h1>
-    <p>This is the Learn More page where you can find more information about our project.</p>
-</div>
-@endsection
